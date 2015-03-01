0ce8bc7bf001d907f8344143534ba8a79a1d07c1
Gary Leung <gleung604@gmail.com>
2015-03-01 14:02:32 -0800

    Add login/logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-
-  get 'users/new'
-
-  root 'static_pages#home'
-
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
-
+  root                'static_pages#home'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :articles
   resources :tags, only: [:show], param: :name
   resources :users
