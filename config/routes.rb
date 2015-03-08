bf07cdefdd2f289dcffc2a02cfb060dddc090f63
Gary Leung <gleung604@gmail.com>
2015-03-07 19:13:32 -0800

    Fix blank links, add content to about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root                'static_pages#home'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
+  get    'popular' => 'static_pages#coming_soon'
+  get    'forums'  => 'static_pages#coming_soon'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
